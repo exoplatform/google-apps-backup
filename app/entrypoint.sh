ef2496e8353c9b636e628257c2421a5327421704
Vincent Sellier <vsellier@Vincents-MacBook-Pro.local>
2016-01-04 10:38:08 +0100

    Remove duplicate execution on entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,7 +2,5 @@
 
 cp /config/settings.yml .
 
-python brive.py -v --keep-on-crash --keep-dirs
-
 # docker run -ti --rm -v $PWD/settings:/config -v $PWD/data:/data brive
 python brive.py -v -d --keep-dirs --keep-on-crash --preferred-formats xlsx docx pdf
